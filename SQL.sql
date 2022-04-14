583b31078fce88d622b998de8d8383f7d1aa9297
AlexWlz <wallezalexis@gmail.com>
2022-04-14 12:00:56 +0200

    commit 'master'

--- SQL.sql
+++ SQL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 01 mars 2022 à 09:47
+-- Généré le : jeu. 14 avr. 2022 à 09:59
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `sats`
+--
+
+DROP TABLE IF EXISTS `sats`;
+CREATE TABLE IF NOT EXISTS `sats` (
+  `number_user` int(11) NOT NULL,
+  `number_school` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `sats`
+--
+
+INSERT INTO `sats` (`number_user`, `number_school`) VALUES
+(3, 4);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `school`
 --
 
@@ -31,6 +50,7 @@ DROP TABLE IF EXISTS `school`;
 CREATE TABLE IF NOT EXISTS `school` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number_user_of_school` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -39,11 +59,11 @@ CREATE TABLE IF NOT EXISTS `school` (
 -- Déchargement des données de la table `school`
 --
 
-INSERT INTO `school` (`id`, `name`) VALUES
-(5, 'college'),
-(3, 'fac'),
-(1, 'lycee'),
-(4, 'sup');
+INSERT INTO `school` (`id`, `name`, `number_user_of_school`) VALUES
+(1, 'lycee', 2),
+(3, 'fac', 2),
+(4, 'sup', 1),
+(5, 'college', 0);
 
 -- --------------------------------------------------------
 
@@ -60,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `admin` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `users`
@@ -69,9 +89,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
 (1, 'gaetan', 'mairui.gaetan@lessonsharing.fr', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'oui'),
 (5, 'myhed.porn', 'myhed@porn.com', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'non'),
-(9, 'nathan', 'lechauve@krilin.jap', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'non'),
-(10, 'romain', 'romain@twigo.com', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'non'),
-(11, 'nicolas', 'nicolas@magicnetwork.com', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'non');
+(9, 'nathan', 'lechauve@krilin.jap', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'non');
 
 -- --------------------------------------------------------
 
@@ -81,9 +99,9 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
 
 DROP TABLE IF EXISTS `user_school_connector`;
 CREATE TABLE IF NOT EXISTS `user_school_connector` (
-  `id_user` int(11) NOT NULL,
-  `id_school` int(11) NOT NULL,
-  PRIMARY KEY (`id_user`,`id_school`),
+  `id_user` int(11) DEFAULT NULL,
+  `id_school` int(11) DEFAULT NULL,
+  `number_intervention` int(11) NOT NULL,
   KEY `id_school` (`id_school`),
   KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -92,12 +110,12 @@ CREATE TABLE IF NOT EXISTS `user_school_connector` (
 -- Déchargement des données de la table `user_school_connector`
 --
 
-INSERT INTO `user_school_connector` (`id_user`, `id_school`) VALUES
-(5, 3),
-(9, 3),
-(10, 3),
-(11, 3),
-(9, 4);
+INSERT INTO `user_school_connector` (`id_user`, `id_school`, `number_intervention`) VALUES
+(5, 1, 0),
+(9, 4, 0),
+(9, 1, 0),
+(9, 3, 0),
+(1, 3, 0);
 
 --
 -- Contraintes pour les tables déchargées
